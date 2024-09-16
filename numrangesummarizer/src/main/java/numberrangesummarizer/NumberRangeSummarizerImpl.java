ef7fe3ee27d594d50c2123524b20aa5ad5e1f5f4
Yedidia Liabara <26259877@sun.ac.za>
2024-09-16 22:16:59 +0200

    implemented the collect function

--- numrangesummarizer/src/main/java/numberrangesummarizer/NumberRangeSummarizerImpl.java
+++ numrangesummarizer/src/main/java/numberrangesummarizer/NumberRangeSummarizerImpl.java
@@ -1,13 +1,32 @@
 package numberrangesummarizer;
 
-import java.util.Collection;
+import java.util.*;
 
 public class NumberRangeSummarizerImpl implements NumberRangeSummarizer {
 
     @Override
     public Collection<Integer> collect(String input) {
+        //Creating an List which will contain/consists of all the numbers
+        List<Integer> nums = new ArrayList<>();
 
-        return null;
+        //Splitting the input string by comma
+        String[] numStrings = input.split(",");
+
+        //Iterating through the splitted strings
+        while (numStrings.length > 0) {
+            //Parsing the string to integer
+            int num = Integer.parseInt(numStrings[0]);
+            //Adding the integer to the list
+            nums.add(num);
+            //Removing the first element from the array
+            numStrings = Arrays.copyOfRange(numStrings, 1, numStrings.length);
+        }
+
+        //Ordering the list, making use of the collections cla
+        //Numerical order sorting from (1,....)
+        Collections.sort(nums);
+
+        return nums;
     }
 
     @Override
