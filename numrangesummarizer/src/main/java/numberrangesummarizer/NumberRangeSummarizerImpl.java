1e067484e416c244d7905685d21670cf0724409b
Yedidia Liabara <26259877@sun.ac.za>
2024-09-18 02:38:31 +0200

    Refactor summarizeCollection method to improve range summarization logic

--- numrangesummarizer/src/main/java/numberrangesummarizer/NumberRangeSummarizerImpl.java
+++ numrangesummarizer/src/main/java/numberrangesummarizer/NumberRangeSummarizerImpl.java
@@ -9,9 +9,6 @@ import java.util.List;
  * This class will implement the NumberRangeSummarizer interface to produce a
  * comma delimited list of numbers, grouping the numbers into a range when they
  * are sequential.
- *
- * Sample Input: "1,3,6,7,8,12,13,14,15,21,22,23,24,31 Result: "1, 3, 6-8,
- * 12-15, 21-24, 31"
  */
 public class NumberRangeSummarizerImpl implements NumberRangeSummarizer {
 
